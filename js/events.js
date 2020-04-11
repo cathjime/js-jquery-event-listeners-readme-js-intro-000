91d157f2d6735a9b3cb4ae05cf0d9afb846f6c2c
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-11 19:00:05 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -15,13 +15,10 @@ function pressIt(){
   $(document).on('keydown', function(key) {
     if(key.which == 71){
       alert('G was pressed');
-  }
-});
+    }
+  });
 }
 
-// should bind a keydown event to the input field 
-// of the form that alerts a user when they have 
-// pressed the G key.
 
 
 $(document).ready(function(){
