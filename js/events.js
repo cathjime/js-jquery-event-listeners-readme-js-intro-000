eb6fd0464e0865e9768bcf4138433710292be9d8
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-11 18:58:41 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt(){
 function pressIt(){
   $(document).on('keydown', function(key) {
     if(key.which == "G"){
-      alert('s was pressed');
+      alert('G was pressed');
   }
 });
 }
